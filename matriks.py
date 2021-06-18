01607cdc8fa2f6b39dc45cd999f89c3dbcb949bc
theman49 <cakdeny49@gmail.com>
2021-06-18 21:44:00 +0700

    update transpose matrix

--- matriks.py
+++ matriks.py
@@ -1,88 +1,24 @@
-import menu
+import myModul
 
-matriksA = []
-matriksB = []
-barisA = 2
-barisB = 6
-kolomA = 4
-kolomB = 3
-print(15*"=", "\n")
+pilihan = myModul.menu(7)
 
-if menu.pilihan == 1 or menu.pilihan == 2:
-    while barisA != barisB and kolomA != kolomB:
-        print("\n", 50*"-", "\n")
-        print("baris dan kolom kedua matriks harus sama")
-        print("masukkan ordo matriks A : \n")
-        barisA = int(input("baris : "))
-        kolomA = int(input("kolom : "))
-
-        print("masukkan ordo matriks B : \n")
-        barisB = int(input("baris : "))
-        kolomB = int(input("kolom : "))
-
-elif menu.pilihan == 3:
-       while kolomA != barisB:
-        print("\n", 50*"-", "\n")
-        print("kolom matriks A harus sama dengan baris matriks B")
-        print("masukkan ordo matriks A : \n")
-        barisA = int(input("baris : "))
-        kolomA = int(input("kolom : "))
-
-        print("masukkan ordo matriks B : \n")
-        barisB = int(input("baris : "))
-        kolomB = int(input("kolom : "))
-
-else:
-    exit()
-
-print(15*"=", "\n")
-print("Masukkan matriks A\n")
-# input matriks A
-for i in range(barisA):
-    list = []
-    for j in range(kolomA):
-        state = f'[{i+1}][{j+1}] : '
-        value = int(input(state))
-        list.append(value)
-    matriksA.append(list)
-    
-print("Masukkan matriks B\n")
-# input matriks B
-for i in range(barisB):
-    list = []
-    for j in range(kolomB):
-        state = f'[{i+1}][{j+1}] : '
-        value = int(input(state))
-        list.append(value)
-    matriksB.append(list)
-
-print(15*"=", "\n")
-print("\nMatriks A (", barisA, "x", kolomA, ") : ")
-for i in range(barisA):
-    string = ""
-    for j in range(kolomA):
-        string += f"{matriksA[i][j]} "
-    print(string, "\n")
-
-print("\nMatriks B (", barisB, "x", kolomB, ") : ")
-for i in range(barisB):
-    string = ""
-    for j in range(kolomB):
-        string += f"{matriksB[i][j]} "
-    print(string, "\n")
-
-
-if menu.pilihan == 1:
-    hasil = menu.penjumlahanMatriks(matriksA, matriksB)
-elif menu.pilihan == 2:
-    hasil = menu.penguranganMatriks(matriksA, matriksB)
-elif menu.pilihan == 3:
-    hasil = menu.perkalianMatriks(matriksA, matriksB)
-
-print("\nHasil : ")
-for i in range(len(hasil)):
-    string = ""
-    for j in range(len(hasil[0])):
-        string += f"{hasil[i][j]} "
-    print(string, "\n")
-print(50*"=")
+while True:
+	if pilihan == 0:
+		exit()
+	elif pilihan == 1:
+		result = myModul.summation()
+	elif pilihan == 2:
+		result = myModul.substraction()
+	elif pilihan == 3:
+		result = myModul.multiplication()
+	elif pilihan == 4:
+		result = myModul.transpose()
+	else:
+		print(25*"=")
+		print("Program wasn't ready")
+		print(25*"=")
+		result = 0
+	if result != 0 :
+		myModul.displayResult(result)
+	
+	pilihan = myModul.menu(7)
---
+++ myModul.py
@@ -0,0 +1,126 @@
+def menu(pilihan):
+	print("Matrix Menu : ")
+	print("0. Exit")
+	print("1. Summation matrix")
+	print("2. Substraction matrix")
+	print("3. Multiplication matrix")
+	print("4. Transpose matrix")
+	print("5. Determine matrix")
+	print("6. Invers matrix")
+
+	while(pilihan > 6):
+	    pilihan = int(input("menu yang dipilih (0-6) : "))
+	return pilihan
+
+def summation():
+    size = inputSize()
+    matrixA = inputMatrix(size)
+    matrixB = inputMatrix(size)    
+	
+    results = []
+    for i in range(len(matrixA)):
+        list = []
+        for j in range(len(matrixB[0])):
+            result = matrixA[i][j] + matrixB[i][j]
+            list.append(result)
+        results.append(list)
+    return results
+
+def substraction():
+    size = inputSize()
+    matrixA = inputMatrix(size)
+    matrixB = inputMatrix(size)    
+
+    results = []
+    for i in range(len(matrixA)):
+        list = []
+        for j in range(len(matrixB[0])):
+            result = matrixA[i][j] - matrixB[i][j]
+            list.append(result)
+        results.append(list)
+    return results
+
+def multiplication():
+	size = inputSize()
+	matrixA = inputMatrix(size)
+	sizeB = inputSize()
+	while size[1] != sizeB[0] :
+		sizeB = inputSize()
+
+	matrixB = inputMatrix(sizeB)    
+
+	results = []
+	for i in range(len(matrixA)):
+		list = []
+		for j in range(len(matrixB[0])):
+			result = 0
+			for k in range(len(matrixA[0])):
+				result += (matrixA[i][k] * matrixB[k][j])
+				list.append(result) 
+		results.append(list)
+	return results
+
+def transpose():
+	size = inputSize()
+	matrix = inputMatrix(size)
+
+	[row, col] = size
+	newMatrix = []
+
+	for i in range(col):
+		tempList = []
+		for j in range(row):
+			tempList.append(matrix[j][i])
+		newMatrix.append(tempList)
+	return newMatrix
+
+def inputSize():
+	print(25*"=")
+	print("input size of matrix: ")
+	print(25*"=")
+	row = int(input("row    : "))
+	col = int(input("column : "))
+
+	return row,col
+
+def inputMatrix(size):
+	print(25*"=")
+	print("input the matrix : ")
+	print(25*"=")
+	matrix = []
+	[row,col] = size
+
+	for i in range(row):
+		tempList = []
+		for j in range(col):
+			str = f'[{i+1}][{j+1}] : '
+			value = int(input(str))
+			tempList.append(value)
+		matrix.append(tempList)
+	displayMatrixInput(matrix)
+	return matrix
+
+def displayMatrixInput(matrix):
+	row = len(matrix)
+	col = len(matrix[0])
+
+	print(25*"=")
+	print("Matriks (", row, "x", col, ") : ")
+	print(25*"=")
+	for i in range(row):
+		str = ""
+		for j in range(col):
+			str += f"{matrix[i][j]} "
+		print(str, "\n")
+
+
+def displayResult(result):
+	print(25*"=")
+	print(f'Result ({len(result)} x {len(result[0])}) : ')
+	print(25*"=")
+	for i in range(len(result)):
+		str = ""
+		for j in range(len(result[0])):
+			str += f"{result[i][j]} "
+		print(str, "\n")
+	print(50*"=")
